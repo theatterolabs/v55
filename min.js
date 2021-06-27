79eace8626c56567d3f8ea775ed2fd370d0adaf9
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2021-06-27 21:51:00 +0530

    Update min.js

--- min.js
+++ min.js
@@ -6811,7 +6811,7 @@ jukebox.Manager = function(e) {
                 h: 64
             },
             playButtonOffset: {
-                x: -1,
+                x: -5,
                 y: 0
             },
             playButtonAlpha: 1,
@@ -6836,7 +6836,7 @@ jukebox.Manager = function(e) {
             infoButtonAnim: new ig.Animation(new ig.AnimationSheet("media/graphics/game/ui/mainmenu/info.png", 54, 56), 0, [0, 1, 2]),
             infoButtonPos: {
                 x: 0,
-                y: 38
+                y: 578
             },
             infoButtonRect: {
                 x: -27,
@@ -6853,7 +6853,7 @@ jukebox.Manager = function(e) {
             moregamesButtonImage: new ig.Image("media/graphics/game/ui/mainmenu/moregames.png"),
             moregamesButtonPos: {
                 x: 41,
-                y: 598
+                y: 578
             },
             moregamesButtonRect: {
                 x: -31,
@@ -7168,7 +7168,7 @@ jukebox.Manager = function(e) {
             control: null,
             pointer: null,
             init: function(e, t, i) {
-                this.parent(e, t, i), this.powerLevelGrad = ig.system.context.createLinearGradient(0, 0, 0, this.powerLevelRect.h), this.powerLevelGrad.addColorStop(0, "#cc1e1c"), this.powerLevelGrad.addColorStop(1, "#f59f2a"), this.pauseAnimUp = new ig.Animation(this.pauseSheet, .1, [0]), this.pauseAnimOver = new ig.Animation(this.pauseSheet, .1, [1]), this.pauseAnimDown = new ig.Animation(this.pauseSheet, .1, [2]), this.pauseAnim = this.pauseAnimUp, this.homeAnimUp = new ig.Animation(this.homeSheet, .1, [0]), this.homeAnimDown = new ig.Animation(this.homeSheet, .1, [1]), this.homeAnim = this.homeAnimUp, this.restartAnimUp = new ig.Animation(this.restartSheet, .1, [0]), this.restartAnimDown = new ig.Animation(this.restartSheet, .1, [1]), this.restartAnim = this.restartAnimUp, this.gameOverAnim = new ig.Animation(this.gameOverAnimSheet, .042, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], !1), this.pauseRect.x = ig.system.width - 58, this.homeRect.x = ig.system.width - 63, this.lifeRect.x = ig.system.width - 158, this.lifeCountPos.x = ig.system.width - 120, this.gameOverTitleRect.x = ig.system.width / 2 - this.gameOverTitleRect.w / 2, this.gameOverPanelRect.x = ig.system.width / 2 - this.gameOverPanelRect.w / 2, this.gameOverBgRect.x = ig.system.width / 2 - this.gameOverBgRect.w / 2, this.panelIconsRect.x = this.gameOverPanelRect.x + 150, this.panelIconsRect.y = this.gameOverPanelRect.y + 121, this.coinEndCountPos.x = this.panelIconsRect.x + 90, this.coinEndCountPos.y = this.panelIconsRect.y + 24, this.cookieEndCountPos.x = this.panelIconsRect.x + 1003, this.cookieEndCountPos.y = this.panelIconsRect.y + 1098, this.homeEndRect.x = ig.system.width / 2 - this.homeEndRect.w + 2000, this.restartEndRect.x = ig.system.width / 2 - this.restartEndRect.w / 2, this.pausedTitlePos.x = ig.system.width / 2 - this.pausedTitleImage.width / 2, this.happyAnim = new ig.Animation(this.happyAnimSheet, .084, [0, 1, 2, 3, 4], !0), this.tutorialTextRect.x = ig.system.width / 2 - this.tutorialTextRect.w / 2, this.happyBgRect.x = this.tutorialTextRect.x - this.happyBgRect.w / 2, this.happyBgRect.y = this.tutorialTextRect.y + this.tutorialTextRect.h / 2 - this.happyBgRect.h / 2, this.tutorialTextRect.w += this.happyBgRect.w / 2
+                this.parent(e, t, i), this.powerLevelGrad = ig.system.context.createLinearGradient(0, 0, 0, this.powerLevelRect.h), this.powerLevelGrad.addColorStop(0, "#cc1e1c"), this.powerLevelGrad.addColorStop(1, "#f59f2a"), this.pauseAnimUp = new ig.Animation(this.pauseSheet, .1, [0]), this.pauseAnimOver = new ig.Animation(this.pauseSheet, .1, [1]), this.pauseAnimDown = new ig.Animation(this.pauseSheet, .1, [2]), this.pauseAnim = this.pauseAnimUp, this.homeAnimUp = new ig.Animation(this.homeSheet, .1, [0]), this.homeAnimDown = new ig.Animation(this.homeSheet, .1, [1]), this.homeAnim = this.homeAnimUp, this.restartAnimUp = new ig.Animation(this.restartSheet, .1, [0]), this.restartAnimDown = new ig.Animation(this.restartSheet, .1, [1]), this.restartAnim = this.restartAnimUp, this.gameOverAnim = new ig.Animation(this.gameOverAnimSheet, .042, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], !1), this.pauseRect.x = ig.system.width - 58, this.homeRect.x = ig.system.width - 63, this.lifeRect.x = ig.system.width - 158, this.lifeCountPos.x = ig.system.width - 120, this.gameOverTitleRect.x = ig.system.width / 2 - this.gameOverTitleRect.w / 2, this.gameOverPanelRect.x = ig.system.width / 2 - this.gameOverPanelRect.w / 2, this.gameOverBgRect.x = ig.system.width / 2 - this.gameOverBgRect.w / 2, this.panelIconsRect.x = this.gameOverPanelRect.x + 150, this.panelIconsRect.y = this.gameOverPanelRect.y + 121, this.coinEndCountPos.x = this.panelIconsRect.x + 90, this.coinEndCountPos.y = this.panelIconsRect.y + 23, this.cookieEndCountPos.x = this.panelIconsRect.x + 1003, this.cookieEndCountPos.y = this.panelIconsRect.y + 1098, this.homeEndRect.x = ig.system.width / 2 - this.homeEndRect.w + 2000, this.restartEndRect.x = ig.system.width / 2 - this.restartEndRect.w / 2, this.pausedTitlePos.x = ig.system.width / 2 - this.pausedTitleImage.width / 2, this.happyAnim = new ig.Animation(this.happyAnimSheet, .084, [0, 1, 2, 3, 4], !0), this.tutorialTextRect.x = ig.system.width / 2 - this.tutorialTextRect.w / 2, this.happyBgRect.x = this.tutorialTextRect.x - this.happyBgRect.w / 2, this.happyBgRect.y = this.tutorialTextRect.y + this.tutorialTextRect.h / 2 - this.happyBgRect.h / 2, this.tutorialTextRect.w += this.happyBgRect.w / 2
             },
             ready: function() {
                 this.control = ig.game.getEntitiesByType(EntityGameControl)[0], this.pointer = ig.game.getEntitiesByType(EntityPointer)[0]
